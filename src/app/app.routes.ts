0c70a03e663f39c902ef5b15cf41e61540aeee58
Mawuli Badassou <mawulibadassou5@gmail.com>
2024-09-13 16:10:15 +0000

    Initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'Home' },
+  { path: 'contact', component: ContactComponent, title: 'Contact' },
+  { path: 'education', component: EducationComponent, title: 'Education' },
+  { path: 'experience', component: ExperienceComponent, title: 'Experience' },
+  { path: 'projects', component: ProjectsComponent, title: 'Projects' },
+  { path: 'skills', component: SkillsComponent, title: 'Skills' },
+];
